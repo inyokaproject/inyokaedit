e1f89a24d386749b6c435cf6f8af656577485a9b
El Thoro <elthoro@gmx.de>
2012-01-07 15:30:17 +0100

    Added new files...

---
+++ CFileOperations.cpp
@@ -0,0 +1,289 @@
+/****************************************************************************
+*
+*   Copyright (C) 2011 by the respective authors (see AUTHORS)
+*
+*   This file is part of InyokaEdit.
+*
+*   InyokaEdit is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   InyokaEdit is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with InyokaEdit.  If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************************/
+
+/****************************************************************************
+* File Name:  CFileOperations.cpp
+* Purpose:    File operations: New, load, save...
+****************************************************************************/
+
+#include "CFileOperations.h"
+
+CFileOperations::CFileOperations(QWidget *pParent, CTextEditor *pEditor, CSettings *pSettings, const QString &sAppName) :
+    m_pParent(pParent),
+    m_pEditor(pEditor),
+    m_pSettings(pSettings),
+    m_sAppName(sAppName)
+{
+    // Generate recent files list
+    mySigMapLastOpenedFiles = new QSignalMapper(this);
+    for (int i = 0; i < m_pSettings->getMaxNumOfRecentFiles(); i++) {
+        if (i < m_pSettings->getRecentFiles().size()) {
+            m_LastOpenedFilesAct << new QAction(m_pSettings->getRecentFiles()[i], this);
+        }
+        else {
+            m_LastOpenedFilesAct << new QAction("EMPTY", this);
+            m_LastOpenedFilesAct[i]->setVisible(false);
+        }
+        mySigMapLastOpenedFiles->setMapping(m_LastOpenedFilesAct[i], i);
+        connect(m_LastOpenedFilesAct[i], SIGNAL(triggered()), mySigMapLastOpenedFiles, SLOT(map()));
+    }
+    connect(mySigMapLastOpenedFiles, SIGNAL(mapped(int)), this, SLOT(openRecentFile(int)));
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+void CFileOperations::newFile()
+{
+    if (this->maybeSave()) {
+        m_pEditor->clear();
+        this->setCurrentFile("");
+    }
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+void CFileOperations::open()
+{
+    if (this->maybeSave()) {
+        QString sFileName = QFileDialog::getOpenFileName(m_pParent, tr("Open file", "GUI: Open file dialog"), m_pSettings->getLastOpenedDir().absolutePath());  // File dialog opens last used folder
+        if (!sFileName.isEmpty()){
+            QFileInfo tmpFI(sFileName);
+            m_pSettings->setLastOpenedDir(tmpFI.absoluteDir());
+            this->loadFile(sFileName);
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+void CFileOperations::openRecentFile(int iEntry) {
+    if (this->maybeSave()) {
+        this->loadFile(m_pSettings->getRecentFiles()[iEntry]);
+    }
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+bool CFileOperations::save()
+{
+    if (m_sCurFile.isEmpty()) {
+        return this->saveAs();
+    } else {
+        return this->saveFile(m_sCurFile);
+    }
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+bool CFileOperations::saveAs()
+{
+    QString sFileName = QFileDialog::getSaveFileName(m_pParent, tr("Save file", "GUI: Save file dialog"), m_pSettings->getLastOpenedDir().absolutePath());  // File dialog opens last used folder
+    if (sFileName.isEmpty())
+        return false;
+
+    QFileInfo tmpFI(sFileName);
+    m_pSettings->setLastOpenedDir(tmpFI.absoluteDir());
+
+    return this->saveFile(sFileName);
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+// Handle unsaved files
+bool CFileOperations::maybeSave()
+{
+    if (m_pEditor->document()->isModified()) {
+        QMessageBox::StandardButton ret;
+        QString sTempCurFileName;
+        if ("" == m_sCurFile){
+            sTempCurFileName = tr("Untitled", "No file name set");
+        }
+        else {
+            QFileInfo tempCurFile(m_sCurFile);
+            sTempCurFileName = tempCurFile.fileName();
+        }
+
+        ret = QMessageBox::warning(m_pParent, m_sAppName,
+                                   tr("The document \"%1\" has been modified.\n"
+                                      "Do you want to save your changes or discard them?", "Msg: Unsaved <sTempCurFileName>").arg(sTempCurFileName),
+                                   QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+        if (QMessageBox::Save == ret)
+            return save();
+        else if (QMessageBox::Cancel == ret)
+            return false;
+    }
+    return true;
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+void CFileOperations::loadFile(const QString &sFileName)
+{
+    QFile file(sFileName);
+    // No permission to read
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(m_pParent, m_sAppName,
+                             tr("The file \"%1\" could not be opened:\n%2.", "Msg: Can not open file, <sFileName>, <ErrorString>")
+                             .arg(sFileName)
+                             .arg(file.errorString()));
+        return;
+    }
+
+    QTextStream in(&file);
+    in.setCodec("UTF-8");
+    in.setAutoDetectUnicode(true);
+
+#ifndef QT_NO_CURSOR
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+#endif
+    m_pEditor->setPlainText(in.readAll());
+#ifndef QT_NO_CURSOR
+    QApplication::restoreOverrideCursor();
+#endif
+
+    this->updateRecentFiles(sFileName);
+    this->setCurrentFile(sFileName);
+    if (m_pSettings->getShowStatusbar()) {
+        emit this->setStatusbarMessage(tr("File loaded"));
+    }
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+bool CFileOperations::saveFile(const QString &sFileName)
+{
+    QFile file(sFileName);
+    // No write permission
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(m_pParent, m_sAppName,
+                             tr("The file \"%1\" could not be saved:\n%2.", "Msg: Can not save file, <sFileName>, <ErrorString>")
+                             .arg(sFileName)
+                             .arg(file.errorString()));
+        return false;
+    }
+
+    QTextStream out(&file);
+    out.setCodec("UTF-8");
+    out.setAutoDetectUnicode(true);
+
+#ifndef QT_NO_CURSOR
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+#endif
+    out << m_pEditor->toPlainText();
+#ifndef QT_NO_CURSOR
+    QApplication::restoreOverrideCursor();
+#endif
+
+    this->updateRecentFiles(sFileName);
+    this->setCurrentFile(sFileName);
+    if (m_pSettings->getShowStatusbar()) {
+        emit this->setStatusbarMessage(tr("File saved"));
+    }
+    return true;
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+QString CFileOperations::getCurrentFile() const {
+    return m_sCurFile;
+}
+
+// ---------------------------------------------------------------------------------------------
+
+void CFileOperations::setCurrentFile(const QString &sFileName)
+{
+    m_sCurFile = sFileName;
+    m_pEditor->document()->setModified(false);
+    m_pParent->setWindowModified(false);
+
+    QString sShownName = m_sCurFile;
+    if (m_sCurFile.isEmpty())
+        sShownName = tr("Untitled");
+    m_pParent->setWindowFilePath(sShownName);
+}
+
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+QList<QAction *> CFileOperations::getLastOpenedFiles() const {
+    return m_LastOpenedFilesAct;
+}
+
+// ---------------------------------------------------------------------------------------------
+
+void CFileOperations::updateRecentFiles(const QString &sFileName) {
+
+    QStringList sListTmp;
+
+    if (sFileName != "_-CL3AR#R3C3NT#F!L35-_") {
+        sListTmp = m_pSettings->getRecentFiles();
+
+        // Remove entry if exists
+        if (sListTmp.contains(sFileName)) {
+            sListTmp.removeAll(sFileName);
+        }
+        // Add file name to list
+        sListTmp.push_front(sFileName);
+
+        // Remove all entries from end, if list is too long
+        while (sListTmp.size() > m_pSettings->getMaxNumOfRecentFiles() || sListTmp.size() > m_pSettings->getNumOfRecentFiles()) {
+            sListTmp.removeLast();
+        }
+
+        for (int i = 0; i < m_pSettings->getMaxNumOfRecentFiles(); i++) {
+            // Set list menu entries
+            if (i < sListTmp.size()) {
+                m_LastOpenedFilesAct[i]->setText(sListTmp[i]);
+                m_LastOpenedFilesAct[i]->setVisible(true);
+            }
+            else {
+                m_LastOpenedFilesAct[i]->setVisible(false);
+            }
+
+        }
+        if (sListTmp.size() > 0) {
+            emit this->setMenuLastOpenedEnabled(true);
+        }
+    }
+
+    // Clear list
+    else {
+        sListTmp.clear();
+        emit this->setMenuLastOpenedEnabled(false);
+    }
+
+    m_pSettings->setRecentFiles(sListTmp);
+}
+
+// ---------------------------------------------------------------------------------------------
+
+void CFileOperations::clearRecentFiles(){
+    this->updateRecentFiles("_-CL3AR#R3C3NT#F!L35-_");
+}
---
+++ CFileOperations.h
@@ -0,0 +1,79 @@
+/****************************************************************************
+*
+*   Copyright (C) 2011 by the respective authors (see AUTHORS)
+*
+*   This file is part of InyokaEdit.
+*
+*   InyokaEdit is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   InyokaEdit is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with InyokaEdit.  If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************************/
+
+/***************************************************************************
+* File Name:  CFileOperations.h
+* Purpose:    Class definition for file operations (new, load, save...)
+***************************************************************************/
+
+#ifndef CFILEOPERATIONS_H
+#define CFILEOPERATIONS_H
+
+#include "CInyokaEdit.h"
+
+class CTextEditor;
+class CSettings;
+
+class CFileOperations : public QObject
+{
+    Q_OBJECT
+
+public:
+    CFileOperations(QWidget *pParent, CTextEditor *pEditor, CSettings *pSettings, const QString &sAppName);
+
+    QString getCurrentFile() const;
+    bool maybeSave();
+
+    QList<QAction *> getLastOpenedFiles() const;
+
+public slots:
+    void newFile();
+    void open();
+    void openRecentFile(int iEntry);
+    void clearRecentFiles();
+    bool save();
+    bool saveAs();
+
+    // Load / save file
+    void loadFile(const QString &sFileName);
+    bool saveFile(const QString &sFileName);
+
+    void setCurrentFile(const QString &sFileName);
+
+signals:
+    void setMenuLastOpenedEnabled(const bool);
+    void setStatusbarMessage(QString);
+
+private:
+    void updateRecentFiles(const QString &sFileName);
+    QSignalMapper *mySigMapLastOpenedFiles;
+
+    QWidget *m_pParent;
+    CTextEditor *m_pEditor;
+    CSettings *m_pSettings;
+
+    QList<QAction *> m_LastOpenedFilesAct;
+
+    QString m_sCurFile;      // Current file
+    QString m_sAppName;
+};
+
+#endif // CFILEOPERATIONS_H
