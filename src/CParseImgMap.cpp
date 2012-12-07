594de9250817b72439f964618b4e4cedff2ba5a2
El Thoro <elthoro@gmx.de>
2012-12-07 18:12:03 +0100

    Added CParseImgMap files...

---
+++ src/CParseImgMap.cpp
@@ -0,0 +1,49 @@
+/**
+ * \file CParseImgMap.cpp
+ *
+ * \section LICENSE
+ *
+ * Copyright (C) 2011-2012 The InyokaEdit developers
+ *
+ * This file is part of InyokaEdit.
+ *
+ * InyokaEdit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * InyokaEdit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with InyokaEdit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * \section DESCRIPTION
+ * Parse all image mapped elents (simple replacement).
+ */
+
+#include <QDebug>
+#include <QRegExp>
+#include <QStringList>
+
+#include "./CParseImgMap.h"
+
+CParseImgMap::CParseImgMap() {
+    qDebug() << "Start" << Q_FUNC_INFO;
+    qDebug() << "End" << Q_FUNC_INFO;
+}
+
+void CParseImgMap::startParsing(QTextDocument *pRawDoc,
+                                QStringList sListFlags,
+                                QStringList sListFlagsImg) {
+    QString sMyDoc = pRawDoc->toPlainText();
+
+    for (int i = 0; i < sListFlags.size(); i++) {
+        sMyDoc.replace(sListFlags[i], "<img src=\"" + sListFlagsImg[i] + "\" />");
+    }
+
+    // Replace raw document with new replaced doc
+    pRawDoc->setPlainText(sMyDoc);
+}
