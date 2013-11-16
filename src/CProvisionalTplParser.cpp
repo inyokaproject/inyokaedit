c4e85279c1a328b0a6f00ed08d1f00dcc3e95ec7
El Thoro <elthoro@gmx.de>
2013-11-16 12:15:20 +0100

    New IWLs, added Trusty.

--- src/CProvisionalTplParser.cpp
+++ src/CProvisionalTplParser.cpp
@@ -1654,7 +1654,10 @@ QString CProvisionalTplParser::parseTested(const QStringList &sListArgs) {
         } else {  // Article tested with ubuntu versions
             for (int i = 0; i < sListArgs.size(); i++) {
                 sOutput += "\n * ";
-                if (sListArgs[i].toLower() == "saucy") {
+                if (sListArgs[i].toLower() == "trusty") {
+                    sOutput += "[:Trusty_Tahr:Ubuntu 14.04] "
+                               "\"Trusty Tahr\"";
+                } else if (sListArgs[i].toLower() == "saucy") {
                     sOutput += "[:Saucy_Salamander:Ubuntu 13.10] "
                                "\"Saucy Salamander\"";
                 } else if (sListArgs[i].toLower() == "raring") {
