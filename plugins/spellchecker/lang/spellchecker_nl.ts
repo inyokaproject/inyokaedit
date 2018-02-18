f18b5c5b1863443768c11ecd56b7704f09e7457b
Heimen Stoffels <vistausss@outlook.com>
2018-02-18 10:37:21 +0100

    Added Dutch plugins translations

---
+++ plugins/spellchecker/lang/spellchecker_nl.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>SpellCheckDialog</name>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="14"/>
+        <source>Spell checker</source>
+        <translation>Spellingcontrole</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="36"/>
+        <source>TextLabel</source>
+        <translation>TekstLabel</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="43"/>
+        <source>Replace with:</source>
+        <translation>Vervangen door:</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="60"/>
+        <source>Unknown word:</source>
+        <translation>Onbekend woord:</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="69"/>
+        <source>Add to dictionary</source>
+        <translation>Toevoegen aan woordenboek</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="76"/>
+        <source>Replace once</source>
+        <translation>Eenmaal vervangen</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="83"/>
+        <source>Replace all</source>
+        <translation>Alles vervangen</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="106"/>
+        <source>Ignore once</source>
+        <translation>Eenmaal negeren</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="113"/>
+        <source>Ignore all</source>
+        <translation>Alles negeren</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="133"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../spellcheckdialog.ui" line="142"/>
+        <source>Language:</source>
+        <translation>Taal:</translation>
+    </message>
+</context>
+<context>
+    <name>SpellChecker</name>
+    <message>
+        <location filename="../spellchecker.cpp" line="115"/>
+        <source>Spell checker</source>
+        <translation>Spellingcontrole</translation>
+    </message>
+    <message>
+        <location filename="../spellchecker.cpp" line="376"/>
+        <source>Spell check has finished.</source>
+        <translation>De spellingcontrole is afgerond.</translation>
+    </message>
+    <message>
+        <location filename="../spellchecker.cpp" line="472"/>
+        <source>Info</source>
+        <translation>Informatie</translation>
+    </message>
+    <message>
+        <location filename="../spellchecker.cpp" line="480"/>
+        <source>Version</source>
+        <translation>Versie</translation>
+    </message>
+    <message>
+        <location filename="../spellchecker.cpp" line="482"/>
+        <source>Licence</source>
+        <translation>Licentie</translation>
+    </message>
+    <message>
+        <location filename="../spellchecker.cpp" line="485"/>
+        <source>Spell checker based on &lt;a href=&quot;http://hunspell.sourceforge.net/&quot;&gt;Hunspell&lt;/a&gt;.</source>
+        <translation>De spellingcontrole is gebaseerd op &lt;a href=&quot;http://hunspell.sourceforge.net/&quot;&gt;Hunspell&lt;/a&gt;.</translation>
+    </message>
+</context>
+</TS>
