a298b57725fe8a789ef5b69671415f666dfd73ab
Heimen Stoffels <vistausss@outlook.com>
2018-02-18 10:36:52 +0100

    Added Dutch plugins translations

---
+++ plugins/hotkey/lang/hotkey_nl.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>Hotkey</name>
+    <message>
+        <location filename="../hotkey.cpp" line="108"/>
+        <source>Hotkey selector</source>
+        <translation>Sneltoetstoewijzing</translation>
+    </message>
+    <message>
+        <location filename="../hotkey.cpp" line="235"/>
+        <source>&apos;&apos;&apos;Bold&apos;&apos;&apos;</source>
+        <translation>&apos;&apos;&apos;Vet&apos;&apos;&apos;</translation>
+    </message>
+    <message>
+        <location filename="../hotkey.cpp" line="383"/>
+        <source>Info</source>
+        <translation>Informatie</translation>
+    </message>
+    <message>
+        <location filename="../hotkey.cpp" line="392"/>
+        <source>Version</source>
+        <translation>Versie</translation>
+    </message>
+    <message>
+        <location filename="../hotkey.cpp" line="394"/>
+        <source>Licence</source>
+        <translation>Licentie</translation>
+    </message>
+    <message>
+        <location filename="../hotkey.cpp" line="397"/>
+        <source>Plugin for defining custom hotkeys.</source>
+        <translation>Plug-in voor het opgeven van aangepaste sneltoetsen.</translation>
+    </message>
+</context>
+<context>
+    <name>HotkeyClass</name>
+    <message>
+        <location filename="../hotkey.ui" line="32"/>
+        <source>Hotkeys</source>
+        <translation>Sneltoetsen</translation>
+    </message>
+</context>
+</TS>
